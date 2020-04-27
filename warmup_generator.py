e223686fb16f9d0f61f698338ed69a25e63e4117
marcusrein <marcus.rein@gmail.com>
2020-04-27 14:55:30 +0000

    pawwwerrr

--- warmup_generator.py
+++ warmup_generator.py
@@ -8,6 +8,7 @@ Created on Fri Apr 24 06:31:13 2020
 print('testerrrr 4/27')
 print('testerrr again')
 print('testerrrrrweeee')
+print('paaawweer')
 import re
 from random import choice
 import random
